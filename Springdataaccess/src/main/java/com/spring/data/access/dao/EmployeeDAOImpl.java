c67e313d31a3193de360843e7d61c9b7faaf952a
Sruthakeerthi Suriya <sruthakeerthis@infinite.com>
2022-11-28 20:51:58 +0530

    Springdataaccess

---
+++ Springdataaccess/src/main/java/com/spring/data/access/dao/EmployeeDAOImpl.java
@@ -0,0 +1,53 @@
+package com.spring.data.access.dao;
+
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import com.spring.data.access.model.Employee;
+import com.spring.data.access.model.EmployeeMapper;
+
+@Component
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+	JdbcTemplate jdbcTemplate;
+	
+	private final String QUERY_FIND_EMPLOYEE_BY_ID = "SELECT * FROM EMPLOYEE WHERE EMP_ID = ?";
+	private final String QUERY_FIND_ALL_EMPLOYEE = "SELECT * FROM EMPLOYEE";
+	private final String QUERY_CREATE_EMPLOYEE = "INSERT INTO EMPLOYEE(EMP_ID,FIRST_NAME, LAST_NAME, ADDRESS, CITY) VALUES(?,?,?,?,?)";
+	private final String QUERY_DELETE_EMPLOYEE = "DELETE FROM EMPLOYEE WHERE EMP_ID = ?";
+	private final String QUERY_UPDATE_EMPLOYEE = "UPDATE EMPLOYEE SET FIRST_NAME = ?, LAST_NAME = ?, ADDRESS = ?, CITY = ? WHERE EMP_ID = ?";
+	
+	
+	@Autowired
+	public EmployeeDAOImpl(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	public Employee getEmployeeById(int id) {
+		return jdbcTemplate.queryForObject(QUERY_FIND_EMPLOYEE_BY_ID, new EmployeeMapper(), id);
+	}
+
+	public List<Employee> getAllEmployees() {
+		return jdbcTemplate.query(QUERY_FIND_ALL_EMPLOYEE, new EmployeeMapper());
+	}
+
+	public boolean createEmployee(Employee emp) {
+		return jdbcTemplate.update(QUERY_CREATE_EMPLOYEE, emp.getId(), emp.getFirstName(), emp.getLastName(), emp.getAddress(), emp.getCity()) > 0;
+	}
+
+	public boolean updateEmployee(Employee emp) {
+		return jdbcTemplate.update(QUERY_UPDATE_EMPLOYEE, emp.getFirstName(), emp.getLastName(), emp.getAddress(), emp.getCity(), emp.getId()) > 0;
+	}
+
+	public boolean deleteEmployee(Employee emp) {
+		return jdbcTemplate.update(QUERY_DELETE_EMPLOYEE, emp.getId()) > 0;
+	}
+
+}
