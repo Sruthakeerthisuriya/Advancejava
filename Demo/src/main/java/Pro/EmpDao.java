f1fb738462e8a5185f3deabaad5539c9ca16561e
Sruthakeerthi Suriya <sruthakeerthis@infinite.com>
2022-12-01 14:17:58 +0530

    IssuePage

---
+++ Demo/src/main/java/Pro/EmpDao.java
@@ -0,0 +1,102 @@
+package Pro;
+
+import java.util.*;  
+import java.sql.*;  
+  
+public class EmpDao {  
+  
+    public static Connection getConnection(){  
+        Connection con=null;  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/sruthi","root","india@123");  
+        }catch(Exception e){System.out.println(e);}  
+        return con;  
+    }  
+    public static int save(Emp e){  
+        int status=0;  
+        try{  
+            Connection con=EmpDao.getConnection();  
+            PreparedStatement ps=con.prepareStatement("insert into userdetails(name,password) values (?,?)");  
+            ps.setString(1,e.getName());  
+            ps.setString(2,e.getPassword());  
+           
+              
+            status=ps.executeUpdate();  
+              
+            con.close();  
+        }catch(Exception ex){ex.printStackTrace();}  
+          
+        return status;  
+    }  
+    public static int update(Emp e){  
+        int status=0;  
+        try{  
+            Connection con=EmpDao.getConnection();  
+            PreparedStatement ps=con.prepareStatement(  
+                         "update userdetails set name=?,password=? where id=?");  
+            ps.setString(1,e.getName());  
+            ps.setString(2,e.getPassword());  
+           
+            ps.setInt(3,e.getId());  
+              
+            status=ps.executeUpdate();  
+              
+            con.close();  
+        }catch(Exception ex){ex.printStackTrace();}  
+          
+        return status;  
+    }  
+    public static int delete(int id){  
+        int status=0;  
+        try{  
+            Connection con=EmpDao.getConnection();  
+            PreparedStatement ps=con.prepareStatement("delete from userdetails where id=?");  
+            ps.setInt(1,id);  
+            status=ps.executeUpdate();  
+              
+            con.close();  
+        }catch(Exception e){e.printStackTrace();}  
+          
+        return status;  
+    }  
+    public static Emp getEmployeeById(int id){  
+        Emp e=new Emp();  
+          
+        try{  
+            Connection con=EmpDao.getConnection();  
+            PreparedStatement ps=con.prepareStatement("select * from userdetails where name=?");  
+            ps.setInt(1,id);  
+            ResultSet rs=ps.executeQuery();  
+            if(rs.next()){  
+                e.setId(rs.getInt(1));  
+                e.setName(rs.getString(2));  
+                e.setPassword(rs.getString(3));  
+                 
+            }  
+            con.close();  
+        }catch(Exception ex){ex.printStackTrace();}  
+          
+        return e;  
+    }  
+    public static List<Emp> getAllEmployees(){  
+        List<Emp> list=new ArrayList<Emp>();  
+          
+        try{  
+            Connection con=EmpDao.getConnection();  
+            PreparedStatement ps=con.prepareStatement("select * from userdetails");  
+            ResultSet rs=ps.executeQuery();  
+            while(rs.next()){  
+                Emp e=new Emp();  
+                e.setId(rs.getInt(1));  
+                e.setName(rs.getString(2));  
+                e.setPassword(rs.getString(3));  
+                 
+                list.add(e);  
+            }  
+            con.close();  
+        }catch(Exception e){e.printStackTrace();}  
+          
+        return list;  
+    }  
+}  
